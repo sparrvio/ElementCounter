f547d64ceae1da3130723c0cb795a23d8cfcfaa8
sparrvio <sparrvio@ya.ru>
2025-02-09 21:40:04 +0300

    CountElements groovy

--- src/ElementCounter.groovy
+++ src/ElementCounter.groovy
@@ -1,13 +1,15 @@
-static void main(String[] args) {}
-
-def countElements(List<Integer> list) {
-  def result = [:]
-  list.each { element ->
-    result[element] = result.getOrDefault(element, 0) + 1
+class CountElements {
+  static void main(String[] args) {
+    def list = [1, 3, 4, 5, 1, 5, 4]
+    def result = countElements(list)
+    println(result)
   }
-  return result
-}
 
-def list = [1, 3, 4, 5, 1, 5, 4]
-def result = countElements(list)
-println(result) // [1:2, 3:1, 4:2, 5:2]
+  static def countElements(List<Integer> list) {
+    def result = [:]
+    list.each { element ->
+      result[element] = result.getOrDefault(element, 0) + 1
+    }
+    return result
+  }
+}
